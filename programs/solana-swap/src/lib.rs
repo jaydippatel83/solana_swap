81b7895af2e17418e4092788ec86122dd936e120
jaydippatel83 <jaydippatel83.pj@gmail.com>
2025-12-16 23:11:01 +0530

    add lib file

--- programs/solana-swap/src/lib.rs
+++ programs/solana-swap/src/lib.rs
@@ -1,31 +1,28 @@
 pub mod constant;
-pub mod instructions;
 pub mod error;
+pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
-use instructions::*;
-use state::*;
 use constant::*;
 use error::*;
-
+use instructions::*;
+use state::*;
 
 declare_id!("Pb2JznuDVXcsHtB5ENj5ku12x1yzuXnyPd1GJXJEUoH");
 
 #[program]
-pub mod solana_swap { 
+pub mod solana_swap {
     use super::*;
 
     pub fn make_offer(
         ctx: Context<MakeOffer>,
-        token_mint_a: Pubkey,
-        token_mint_b: Pubkey,
+        id: u64,
+        token_a_offered_amount: u64,
         token_b_wanted_amount: u64,
-        ) -> Result<()> {
-        instructions::make_offer::send_offered_tokens_to_vault()?;
-        instructions::make_offer::save_offer();
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?; 
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)?;
+        Ok(())
     }
 }
-
-#[derive(Accounts)]
-
